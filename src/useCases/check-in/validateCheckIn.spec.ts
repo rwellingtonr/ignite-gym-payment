77b562e1038c6ec8f56b29ba495b0ae8d634e296
Wellington <wellington-158@hotmail.com>
2023-04-29 08:33:38 -0300

    test: validação de check-in dentro de 20min

--- src/useCases/check-in/validateCheckIn.spec.ts
+++ src/useCases/check-in/validateCheckIn.spec.ts
@@ -1,18 +1,15 @@
 import { afterEach, beforeEach, describe, it, expect, vi } from "vitest"
 import { ICheckInRepository } from "~/infra/repository/check-in/interface"
 import { CheckInRepositoryInMemory } from "~/infra/repository/in-memory/checkInRepositoryInMemory"
-import { faker } from "@faker-js/faker"
 import { ValidateCheckInService } from "./validateCheckIn"
 import { randomUUID } from "crypto"
 
 describe("Validate check-in Service", () => {
 	let checkInRepository: ICheckInRepository
-
 	let sut: ValidateCheckInService
 
 	beforeEach(() => {
 		checkInRepository = new CheckInRepositoryInMemory()
-
 		sut = new ValidateCheckInService(checkInRepository)
 		vi.useFakeTimers()
 	})
@@ -40,5 +37,18 @@ describe("Validate check-in Service", () => {
 		})
 
 		await expect(() => sut.execute({ checkInId: randomUUID() })).rejects.toThrow()
+	})
+
+	it("Should not be able to validate check-in after 20 minutes", async () => {
+		vi.setSystemTime(new Date(2023, 0, 20, 8, 10, 15))
+		const createCheckIn = {
+			gymId: randomUUID(),
+			userId: "random-id",
+		}
+		const { id } = await checkInRepository.create(createCheckIn)
+		const twentyOneMinutes = 1000 * 60 * 21
+		vi.advanceTimersByTime(twentyOneMinutes)
+
+		await expect(() => sut.execute({ checkInId: id })).rejects.toThrow()
 	})
 })
