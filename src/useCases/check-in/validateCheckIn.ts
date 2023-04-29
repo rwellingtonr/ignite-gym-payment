90bc153d463bd190ee3d4059eb73b9688bbbaf5d
Wellington <wellington-158@hotmail.com>
2023-04-29 08:33:38 -0300

    feat: validação de check-in dentro de 20min

--- src/useCases/check-in/validateCheckIn.ts
+++ src/useCases/check-in/validateCheckIn.ts
@@ -1,4 +1,5 @@
 import { CheckIn } from "@prisma/client"
+import dayjs from "dayjs"
 import { makeError } from "~/helpers/errors"
 import { ICheckInRepository } from "~/infra/repository/check-in/interface"
 import { IGymRepository } from "~/infra/repository/gym/interface"
@@ -21,6 +22,17 @@ export class ValidateCheckInService {
 			const Error404 = makeError("404", "Could not find this check-in")
 			throw new Error404()
 		}
+
+		const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+			checkIn.createdAt,
+			"minutes",
+		)
+
+		if (distanceInMinutesFromCheckInCreation > 20) {
+			const Error403 = makeError("403", "Check-in can only be validated within 20 minutes")
+			throw Error403
+		}
+
 		checkIn.validatedAt = new Date()
 		const checkInUpdated = await this.checkInRepository.update(checkIn)
 
