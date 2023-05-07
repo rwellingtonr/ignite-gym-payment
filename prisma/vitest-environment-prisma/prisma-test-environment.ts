58bc17c593e62ea5d1963747384e94c7caf07f9f
Wellington <wellington-158@hotmail.com>
2023-05-07 09:52:52 -0300

    test: preparado env de e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,40 @@
+import "dotenv/config"
+import { randomUUID } from "node:crypto"
+import { execSync } from "node:child_process"
 import type { Environment } from "vitest"
+import { PrismaClient } from "@prisma/client"
+
+const prismaClient = new PrismaClient()
+
+async function deleteDataBaseSchema(schema: string) {
+	await prismaClient.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+	await prismaClient.$disconnect()
+}
+
+function generateDBUrl(nameSchema: string) {
+	if (process.env.DATABASE_URL) {
+		throw new Error("Please provide de database url!")
+	}
+
+	const url = new URL(process.env.DATABASE_URL as string)
+	url.searchParams.set("schema", nameSchema)
+
+	return url.toString()
+}
 
 export default <Environment>{
 	name: "prisma",
 	async setup() {
-		// custom setup
-		console.log("My Custom Environment")
+		const schemaName = randomUUID()
+		const databaseUrl = generateDBUrl(schemaName)
+
+		process.env.DATABASE_URL = databaseUrl
+
+		execSync("npx prisma migrate deploy")
 
 		return {
-			teardown() {
-				// called after all tests with this env have been run
+			async teardown() {
+				await deleteDataBaseSchema(schemaName)
 			},
 		}
 	},
